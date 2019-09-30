ecfe5ef9fadb3ff3a5dfa55ea6bd529c46a1765f
Matheus Henrique de Sousa Cruz <matheus.cruz@radixeng.com.br>
2019-09-30 16:23:46 -0300

    Adicionando url

--- src/app/www/js/service.js
+++ src/app/www/js/service.js
@@ -1,6 +1,6 @@
 async function emailService(email) {
     $.ajax({
-        url: "http://localhost:3000/email/notify",
+        url: "https://poc-radix.herokuapp.com/email/notify",
         dataType: "JSON",
         contentType: "application/json",
         data: JSON.stringify(email),
@@ -10,7 +10,7 @@ async function emailService(email) {
 
 async function addCommentService(comment) {
     $.ajax({
-        url: `http://localhost:3000/documents/${comment.doc}/threads/${comment.threadId}/comments`,
+        url: `https://poc-radix.herokuapp.com/documents/${comment.doc}/threads/${comment.threadId}/comments`,
         dataType: "JSON",
         contentType: "application/json",
         data: JSON.stringify(comment),
@@ -20,26 +20,26 @@ async function addCommentService(comment) {
 
 async function getParticipantsFromDocument(documentId) {
     const data = await $.get(
-        `http://localhost:3000/documents/${documentId}/participants`
+        `https://poc-radix.herokuapp.com/documents/${documentId}/participants`
     );
     return data.participants;
 }
 
 async function getTopicFromDocument(documentId, topicId) {
     const data = await $.get(
-        `http://localhost:3000/documents/${documentId}/topics/${topicId}`
+        `https://poc-radix.herokuapp.com/documents/${documentId}/topics/${topicId}`
     );
     return data.topic;
 }
 
 async function getAllReferences() {
-    const data = await $.get("http://localhost:3000/references");
+    const data = await $.get("https://poc-radix.herokuapp.com/references");
     return data.references;
 }
 
 async function updateTopicData(topic) {
     const data = await $.ajax({
-        url: `http://localhost:3000/topics/${topic.id}`,
+        url: `https://poc-radix.herokuapp.com/topics/${topic.id}`,
         dataType: "JSON",
         contentType: "application/json",
         data: JSON.stringify(topic),
@@ -51,7 +51,7 @@ async function updateTopicData(topic) {
 
 async function deleteComment(comment) {
     const data = await $.ajax({
-        url: `http://localhost:3000/documents/66c4ab1b-284e-4f5a-b18a-f83101d8ec0a/threads/${comment.threadId}/comments/${comment.commentId}`,
+        url: `https://poc-radix.herokuapp.com/documents/66c4ab1b-284e-4f5a-b18a-f83101d8ec0a/threads/${comment.threadId}/comments/${comment.commentId}`,
         method: "DELETE"
     })
     return data;
@@ -59,7 +59,7 @@ async function deleteComment(comment) {
 
 async function updateComment(comment) {
     const data = await $.ajax({
-        url: `http://localhost:3000/documents/66c4ab1b-284e-4f5a-b18a-f83101d8ec0a/threads/${comment.threadId}/comments/${comment.commentId}`,
+        url: `https://poc-radix.herokuapp.com/documents/66c4ab1b-284e-4f5a-b18a-f83101d8ec0a/threads/${comment.threadId}/comments/${comment.commentId}`,
         method: "PUT",
         dataType: "JSON",
         contentType: "application/json",
--- src/mock-repository/references.js
+++ src/mock-repository/references.js
@@ -5,70 +5,70 @@ const getMockReferences = () => {
             technical_name: "TECHNICAL-NAME-000",
             document_reference_id: "15092301-ba3a-4f70-ae5b-0ef57191b0e7",
             link:
-                "http://localhost:3000/references/15092301-ba3a-4f70-ae5b-0ef57191b0e7"
+                "https://poc-radix.herokuapp.com/references/15092301-ba3a-4f70-ae5b-0ef57191b0e7"
         },
         {
             id: ":R4D1X-DOC-111",
             technical_name: "TECHNICAL-NAME-111",
             document_reference_id: "b6a649a7-83eb-4024-a504-e2882121a766",
             link:
-                "http://localhost:3000/references/b6a649a7-83eb-4024-a504-e2882121a766"
+                "https://poc-radix.herokuapp.com/references/b6a649a7-83eb-4024-a504-e2882121a766"
         },
         {
             id: ":R4D1X-DOC-222",
             technical_name: "TECHNICAL-NAME-222",
             document_reference_id: "7b3005df-d1b7-4758-8206-dbe779fa1682",
             link:
-                "http://localhost:3000/references/7b3005df-d1b7-4758-8206-dbe779fa1682"
+                "https://poc-radix.herokuapp.com/references/7b3005df-d1b7-4758-8206-dbe779fa1682"
         },
         {
             id: ":R4D1X-DOC-333",
             technical_name: "TECHNICAL-NAME-333",
             document_reference_id: "b160be53-1b82-4b1a-9410-01cfd42215b1",
             link:
-                "http://localhost:3000/references/b160be53-1b82-4b1a-9410-01cfd42215b1"
+                "https://poc-radix.herokuapp.com/references/b160be53-1b82-4b1a-9410-01cfd42215b1"
         },
         {
             id: ":R4D1X-DOC-444",
             technical_name: "TECHNICAL-NAME-444",
             document_reference_id: "b84f2b86-b2ea-4348-8090-72c26bf3736d",
             link:
-                "http://localhost:3000/references/b84f2b86-b2ea-4348-8090-72c26bf3736d"
+                "https://poc-radix.herokuapp.com/references/b84f2b86-b2ea-4348-8090-72c26bf3736d"
         },
         {
             id: ":R4D1X-DOC-555",
             technical_name: "TECHNICAL-NAME-555",
             document_reference_id: "cd49beba-f1ad-4a53-abc5-59c2bc8dba59",
             link:
-                "http://localhost:3000/references/cd49beba-f1ad-4a53-abc5-59c2bc8dba59"
+                "https://poc-radix.herokuapp.com/references/cd49beba-f1ad-4a53-abc5-59c2bc8dba59"
         },
         {
             id: ":R4D1X-DOC-666",
             technical_name: "TECHNICAL-NAME-666",
             document_reference_id: "2b8e9c0c-72e7-4bd3-846d-944e5583a5cd",
             link:
-                "http://localhost:3000/references/2b8e9c0c-72e7-4bd3-846d-944e5583a5cd"
+                "https://poc-radix.herokuapp.com/references/2b8e9c0c-72e7-4bd3-846d-944e5583a5cd"
         },
         {
             id: ":R4D1X-DOC-777",
             technical_name: "TECHNICAL-NAME-777",
             document_reference_id: "02d38d92-596c-4207-90b5-7820d0326c59",
             link:
-                "http://localhost:3000/references/02d38d92-596c-4207-90b5-7820d0326c59"
+                "https://poc-radix.herokuapp.com/references/02d38d92-596c-4207-90b5-7820d0326c59"
         },
         {
             id: ":R4D1X-DOC-888",
             technical_name: "TECHNICAL-NAME-888",
             document_reference_id: "6d0946e6-58d6-42ff-9e55-3b97a176e00f",
             link:
-                "http://localhost:3000/references/6d0946e6-58d6-42ff-9e55-3b97a176e00f"
+                "https://poc-radix.herokuapp.com/references/6d0946e6-58d6-42ff-9e55-3b97a176e00f"
         },
         {
             id: ":R4D1X-DOC-999",
             technical_name: "TECHNICAL-NAME-999",
             document_reference_id: "b81f38f8-6ccb-430a-bc41-5163f32712fa",
             link:
-                "http://localhost:3000/references/b81f38f8-6ccb-430a-bc41-5163f32712fa"
+                "https://poc-radix.herokuapp.com/references/b81f38f8-6ccb-430a-bc41-5163f32712fa"
         }
     ]
 }
